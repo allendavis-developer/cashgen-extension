fc5239a494ae4551e31afc41214928d5292bad3c
Allen Davis <allendavis.pr@gmail.com>
2026-01-31 20:02:09 +0000

    can't even remember what this change was

--- background.js
+++ background.js
@@ -582,12 +582,12 @@ async function handleScrapeRequest(data, sendResponse) {
 
         url = directUrl;
 
-        // // 👇 Append supported eBay filters
-        // url = applyEbayFilters(url, {
-        //   ebayFilterSold,
-        //   ebayFilterUsed,
-        //   ebayFilterUKOnly
-        // });
+        // 👇 Append supported eBay filters
+        url = applyEbayFilters(url, {
+          ebayFilterSold,
+          ebayFilterUsed,
+          ebayFilterUKOnly
+        });
       } else {
         url = typeof config.searchUrl === "function"
           ? config.searchUrl({
